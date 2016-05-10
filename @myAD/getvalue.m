ca7a5be1db03c131124bbaef233f2a9912f6649a
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2016-05-10 16:30:34 -0400

    Removed unnecessary files

--- @myAD/getvalue.m
+++
@@ -1,7 +0,0 @@
-function val = getvalue(x)
-% In Package myAD - Automatic Differentiation
-% by Martin Fink, June 2006
-% martinfink 'at' gmx.at
-
-    val = x.values;
-
--- @myAD/set.m
+++
@@ -1,6 +0,0 @@
-function set(x, varargin)
-% In Package myAD - Automatic Differentiation
-% by Martin Fink, May 2007
-% martinfink 'at' gmx.at
-
-display(x, inputname(1));
