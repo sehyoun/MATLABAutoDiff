aad79bfc511b5b7148feec82981a143b213a0c06
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2016-08-05 19:35:36 -0400

    Updated transposes to handle large sparse valued matrices better

--- @myAD/ctranspose.m
+++ @myAD/ctranspose.m
@@ -1,8 +1,13 @@
 function x=ctranspose(x)
 % by SeHyoun Ahn, Jan 2016
+
 % Only allows real derivatives, so this does not conjugate derivative
 % values
-[n,m]=size(x.values);
-p=reshape(reshape(1:n*m,n,m)',n*m,1);
-x.derivatives=x.derivatives(p,:);
+
+[n,m] = size(x.values);
+l = size(x.derivatives,2);
+[i,j,v] = find(x.derivatives);
+aux1 = mod(i,n);
+aux1(aux1==0) = n;
+x.derivatives=sparse(((aux1-1)*m+ceil(i/n)),j,v,m*n,l);
 x.values=x.values';
