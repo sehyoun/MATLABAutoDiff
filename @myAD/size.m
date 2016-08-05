2d10b68de26ea23fe82dfaac1f44d1c3ce56da9c
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2016-08-05 19:33:15 -0400

    Updated size to handle variable number of outputs correctly.

--- @myAD/size.m
+++ @myAD/size.m
@@ -1,3 +1,8 @@
 function varargout = size(x, varargin)
 % by SeHyoun Ahn, July 2016
-varargout = {size(x.values,varargin{:})};
+if nargout<=1
+    varargout = {size(x.values,varargin{:})};
+else
+    aux = size(x.values,varargin{:});
+    varargout = mat2cell(aux(1:nargout),1,ones(nargout,1));
+end
