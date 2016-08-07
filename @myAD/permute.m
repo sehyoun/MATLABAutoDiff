f40653e90088822fa53f482cda9b91b29afe17ed
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2016-08-07 08:12:39 -0400

    Improved behavior of repmat and permute with large sparse matrices of dual numbers

--- @myAD/permute.m
+++ @myAD/permute.m
@@ -1,6 +1,27 @@
 function x=permute(x,order)
 % by SeHyoun Ahn, July 2016
-aux=size(x.values);
-p=permute(reshape(1:prod(aux),aux),order);
-x.derivatives=x.derivatives(p(:),:);
-x.values=permute(x.values,order);
+
+if ~issparse(x.values)
+    aux=size(x.values);
+    p=permute(reshape(1:prod(aux),aux),order);
+    x.derivatives=x.derivatives(p(:),:);
+    x.values=permute(x.values,order);
+else
+    if (size(order) == [1,2])
+        if (order == [2,1])
+            x = x';
+        elseif (order == [1,2])
+        else
+            error('ORDER contains an invalid permutation; try using transpose instead.');
+        end
+    elseif (size(order) == [2,1])
+        if (order == [2;1])
+            x=x';
+        elseif (order == [1;2])
+        else
+            error('ORDER contains an invalid permutation; try using transpose instead.');
+        end
+    else
+        error('ORDER contains an invalid permutation; try using transpose instead.');
+    end
+end
