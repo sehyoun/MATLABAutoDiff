f25c6a9f23beaec5ad32cbd88569b08a51a2bd45
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2016-08-11 12:22:44 -0400

    Updated transpose/ctranspose to use dertransp.c

--- @myAD/ctranspose.m
+++ @myAD/ctranspose.m
@@ -5,9 +5,13 @@ function x=ctranspose(x)
 % values
 
 [n,m] = size(x.values);
-l = size(x.derivatives,2);
-[i,j,v] = find(x.derivatives);
-aux1 = mod(i,n);
-aux1(aux1==0) = n;
-x.derivatives=sparse(((aux1-1)*m+ceil(i/n)),j,v,m*n,l);
+try 
+  x.derivatives = dertransp(x.derivatives,n);
+catch
+  l = size(x.derivatives,2);
+  [i,j,v] = find(x.derivatives);
+  aux1 = mod(i,n);
+  aux1(aux1==0) = n;
+  x.derivatives=sparse(((aux1-1)*m+ceil(i/n)),j,v,m*n,l);
+end
 x.values=x.values';
