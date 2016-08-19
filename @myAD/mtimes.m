0c7b9eb633d3254739311f56d85177512f090b2e
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2016-08-19 13:14:44 -0400

    Updated mtimes.m to use matvalXmatder for mat-mat multiplication

--- @myAD/mtimes.m
+++ @myAD/mtimes.m
@@ -1,20 +1,20 @@
 function x = mtimes(x, y)
 % by SeHyoun Ahn, Jan 2016
+
 % Note that in the original package by Martin Fink, this redirected to element-wise multiplication
+
 if isa(x,'myAD')
     [n,m]=size(x.values);
     if isa(y,'myAD')
         if m>1 && size(y,1)==m
-            if size(y,2)>1
-                z = myAD(x.values*y.values,sparse(n*size(y,2),size(x.derivatives,2)));
-                for j=1:size(y,2)
-                    z.derivatives((j-1)*n+(1:n),:)=sparse(x.values)*y.derivatives((j-1)*m+(1:m),:) + matdrivXvecval(x.derivatives,y.values(:,j));
-                end
-                x=z;
+            if (size(y,2)==1)
+                x.derivatives = sparse(x.values)*y.derivatives...
+                                +matdrivXvecval(x.derivatives,y.values);
             else
-                x.derivatives = sparse(x.values)*y.derivatives + matdrivXvecval(x.derivatives,y.values);
-                x.values=x.values*y.values;
+                x.derivatives = matvalXmatder(sparse(x.values),dertransp(y.derivatives,m))...
+                           +dertransp(matvalXmatder(sparse(y.values'),x.derivatives),size(y,2));
             end
+            x.values = x.values*y.values;
         elseif max(m,n)==1
             x.derivatives= valXder(y.values(:),x.derivatives)+y.derivatives*x.values;
             x.values=x.values*y.values;
@@ -26,16 +26,8 @@ if isa(x,'myAD')
         end
     else
         if m>1 && size(y,1)==m
-            if size(y,2)>1
-                z = myAD(x.values*y,sparse(n*size(y,2),size(x.derivatives,2)));
-                for j=1:size(y,2)
-                    z.derivatives((j-1)*n+(1:n),:)=matdrivXvecval(x.derivatives,y(:,j));
-                end
-                x=z;
-            else
-                x.derivatives = matdrivXvecval(x.derivatives,y);
-                x.values=x.values*y;
-            end
+            x.derivatives = dertransp(matvalXmatder(sparse(y'),x.derivatives),size(y,2));
+            x.values = x.values*y;
         elseif max(m,n)==1
             x.derivatives= valXder(y(:),x.derivatives);
             x.values=x.values*y;
@@ -49,17 +41,9 @@ if isa(x,'myAD')
 else
     [n,m]=size(x);
     if m>1 && size(y,1)==m
-        if size(y,2)>1
-            z = myAD(x*y.values,sparse(n*size(y,2),size(y.derivatives,2)));
-            for j=1:size(y,2)
-                z.derivatives((j-1)*n+(1:n),:)=sparse(x)*y.derivatives((j-1)*m+(1:m),:);
-            end
-            x=z;
-        else
-            y.derivatives = sparse(x)*y.derivatives;
-            y.values=x*y.values;
-            x=y;
-        end
+        y.derivatives = matvalXmatder(sparse(x'),dertransp(y.derivatives,m));
+        y.values = x*y.values;
+        x=y;
     elseif max(m,n)==1
         y.derivatives=y.derivatives*x;
         y.values=x*y.values;
