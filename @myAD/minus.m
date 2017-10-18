7d9728b291f5d33e2ad4c116c3af59d0c93e400e
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2017-10-18 18:04:31 +0200

    Fixed <plus> and <minus> to handle vector/matrix +/- scaler correctly

--- @myAD/minus.m
+++ @myAD/minus.m
@@ -1,11 +1,19 @@
 function x = minus(x,y)
+% Updated by SeHyoun Ahn, Oct 2017
+
 % In Package myAD - Automatic Differentiation
 % by Martin Fink, June 2006
 % martinfink 'at' gmx.at
 if isa(x, 'myAD')
     if isa(y, 'myAD')
         x.values = x.values - y.values;
-        x.derivatives = x.derivatives - y.derivatives;
+        if numel(x) == numel(y)
+            x.derivatives = x.derivatives - y.derivatives;
+        elseif (numel(y) == 1) || (numel(x) == 1)
+            x.derivatives = bsxfun(@minus, x.derivatives, y.derivatives);
+        else
+            error('Implicit expansion is not supported yet!');
+        end
     else
         x.values = x.values - y;
     end
--- @myAD/plus.m
+++ @myAD/plus.m
@@ -1,11 +1,19 @@
 function x = plus(x, y)
+% Updated by SeHyoun Ahn, Oct 2017
+
 % In Package myAD - Automatic Differentiation
 % by Martin Fink, June 2006
 % martinfink 'at' gmx.at
 if isa(x, 'myAD')
     if isa(y, 'myAD')
         x.values = x.values + y.values;
-        x.derivatives = x.derivatives + y.derivatives;
+        if numel(x) == numel(y)
+            x.derivatives = x.derivatives + y.derivatives;
+        elseif (numel(x) == 1) || (numel(y) == 1)
+            x.derivatives = bsxfun(@plus, x.derivatives, y.derivatives);
+        else
+            error('Implicit Expansion is not supported yet!');
+        end
     else
         x.values = x.values + y;
     end
