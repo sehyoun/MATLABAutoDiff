63b248a0b426637ff822da64c0c12eba15d0e2ea
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2019-01-10 10:38:59 +0100

    Fixed isnan reshaping

--- @myAD/isnan.m
+++ @myAD/isnan.m
@@ -1,5 +1,8 @@
 function y = isnan(x)
-% In Package myAD - Automatic Differentiation
-% by Martin Fink, June 2006
-% martinfink 'at' gmx.at
-y = isnan(x.values) | isnan(sum(x.derivatives,2));
+  % In Package myAD - Automatic Differentiation
+  % by Martin Fink, June 2006
+  % martinfink 'at' gmx.at
+
+  % Updated by SeHyoun, Jan 2019
+  y = isnan(x.values) | reshape(isnan(sum(x.derivatives,2)), size(x.values));
+end
